edc5e22ab002b6533f7301ac7d89c1d8013d0f66
Vaden <michael.vaden@delta.com>
2023-09-13 13:14:36 -0400

    working through getting ExcelConnection to remotely open Excel files

--- src/ExcelConnection.py
+++ src/ExcelConnection.py
@@ -1,8 +1,11 @@
-import win32com.client
+import win32com.client as win32
+import os
 
 
 class ExcelConnection:
 	def __init__(self) -> None:
-		excel = win32com.client.gencache.EnsureDispatch('Excel.Application')
+		excel = win32.gencache.EnsureDispatch('Excel.Application')
+		wb = excel.Workbooks.Open(f'{os.getcwd()}\\test.xlsx')
 		print(excel.Workbooks.Count)
-
+		print(os.getcwd())
+		
