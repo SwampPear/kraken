02d25a1198a2fe9ec0c44a78c47b8095e7897643
Vaden <michael.vaden@delta.com>
2023-09-13 12:42:54 -0400

    cleaned up 'init_server_def' function in SASConnection

--- src/SASConnection.py
+++ src/SASConnection.py
@@ -10,16 +10,12 @@ class SASConnection:
 
 	def init_server_def(self):
 		server_def = win32com.client.Dispatch('SASObjectManager.ServerDef')
-		#server_def.MachineDNSName = "servername"
-		#server_def.Port = 8591    # workspace server port
-		#server_def.Protocol = 2   # 2 = IOM protocol
-		#server_def.BridgeSecurityPackage = "Username/Password"
-		#server_def.ClassIdentifier = "workspace server id"
-		server_def.MachineDNSName = os.getenv('SERVER')
-		server_def.Port = os.getenv('SERVER_PORT')
-		server_def.Protocol = os.getenv('IOM_PROTOCOL')
-		server_def.BridgeSecurityPackage = os.getenv('USERNAME_PASSWORD')
-		server_def.ClassIdentifier = os.getenv('SERVER_ID')
+		
+		server_def.MachineDNSName = os.getenv('SERVER')			# server name
+		server_def.Port = os.getenv('SERVER_PORT')				# workspace server port
+		server_def.Protocol = os.getenv('IOM_PROTOCOL')			# 2 = IOM protocol
+		server_def.BridgeSecurityPackage = os.getenv('UP')		# username/password
+		server_def.ClassIdentifier = os.getenv('SERVER_ID')		# workspace server id
 
 		return server_def
 
