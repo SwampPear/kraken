96b14844b5c36458f82bae7239dab9b3568b60cc
Vaden <michael.vaden@delta.com>
2023-09-13 12:34:20 -0400

    implemented creation of win32 SAS app

--- src/SASConnection.py
+++ src/SASConnection.py
@@ -1,5 +1,22 @@
 import win32com.client
 
+
 class SASConnection:
 	def __init__(self) -> None:
-		self.obj_factory = win32com.client.Dispatch("SASObjectManager.ObjectFactoryMulti2")
+		self.factory = win32com.client.Dispatch("SASObjectManager.ObjectFactoryMulti2")
+		self.server_def = self.init_server_def()
+		self.sas = self.init_sas
+
+	def init_server_def(self):
+		server_def = win32com.client.Dispatch("SASObjectManager.ServerDef")
+		#server_def.MachineDNSName = "servername"
+		#server_def.Port = 8591    # workspace server port
+		#server_def.Protocol = 2   # 2 = IOM protocol
+		#server_def.BridgeSecurityPackage = "Username/Password"
+		#server_def.ClassIdentifier = "workspace server id"
+
+		return server_def
+
+	def init_sas(self):
+		return self.factory.CreateObjectByServer("SASApp", True, self.server_def, "uid", "pw")
+
